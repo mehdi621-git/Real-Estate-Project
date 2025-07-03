72effbc4e6a5bb269a903500cd24767710f64137
mehdi621-git <mehdisn621@gmail.com>
2025-07-03 10:06:13 +0500

    created listing model and configure backend

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import  userRouter from './routes/user.route.js';
 import authRoute from './routes/auth.route.js';
 import cloudinaryRoute from './routes/imgupload.route.js'
 import updationRoute from './routes/updation.route.js'
+import listingRoute from './routes/listing.route.js'
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -26,6 +27,7 @@ app.use('/server/user',userRouter)
 app.use('/server/auth',authRoute)
 app.use('/server/cloudinary',cloudinaryRoute)
 app.use('/server/updation',updationRoute)
+app.use('/server/listing',listingRoute)
 
 app.use((err,req,res,next)=>{
     const statuscode = err.statusCode || err.code  || 500;
--- server/routes/listing.route.js
+++ server/routes/listing.route.js
@@ -1,9 +1,9 @@
 import express from 'express'
 import { createListing } from '../controllers/listing.controller.js';
-import { validateUser } from '../utils/validateUser.js';
+
 
 const route= express.Router();
 
 
-route.post('/create',validateUser,createListing)
+route.post('/create',createListing)
 export default route
